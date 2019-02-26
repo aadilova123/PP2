5c994dd2ebc00b7f4dc7e0128bd7841a0ab06726
Ayana Adilova <aadilova19@gmail.com>
2019-02-26 17:58:39 +0600

    Task1

---
+++ TSIS4/Task1/Task1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Task1
+{
+    
+    public class ComplexNumbers
+    {
+        public int a;
+        public int b;
+        public string i;
+        public ComplexNumbers(int a , int b )
+        {
+            this.a = a;
+            this.b = b;
+           
+        }
+        public ComplexNumbers()
+        {
+
+        }
+        public override string ToString()
+        {
+
+            return(string.Format("{0} + {1}i", a, b));
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("complex.xml", FileMode.Create, FileAccess.Write);
+            ComplexNumbers complex = new ComplexNumbers(5, 7);
+
+
+            XmlSerializer xs = new XmlSerializer(typeof(ComplexNumbers));
+            xs.Serialize(fs, complex);
+            fs.Close();
+            FileStream fs1 = new FileStream("complex.xml", FileMode.Open, FileAccess.Read);
+            XmlSerializer xml = new XmlSerializer(typeof(ComplexNumbers));
+            ComplexNumbers c = xml.Deserialize(fs1) as ComplexNumbers;
+            Console.WriteLine(c);
+            fs1.Close();
+        }
+        
+        
+    }
+}
---
+++ TSIS4/Task2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Marks
+    {
+        public string Mark;
+
+        public Marks()
+        {
+            
+        }
+        public Marks(int point)
+        {
+            if (point >= 0 || point <=49)
+            {
+                Console.WriteLine("F");
+            }
+            else if (point >= 50 || point <= 54)
+            {
+                Console.WriteLine("D");
+            }
+            else if(point >=55 || point <= 59)
+            {
+                Console.WriteLine("D+");
+            }
+            else if (point >= 60 || point <= 64)
+            {
+                Console.WriteLine("C-");
+            }
+            else if (point >= 65 || point <= 69)
+            {
+                Console.WriteLine("C");
+            }
+            else if (point >= 70 || point <= 74)
+            {
+                Console.WriteLine("C+");
+            }
+            else if (point >= 75 || point <= 79)
+            {
+                Console.WriteLine("B-");
+            }
+            else if (point >= 80 || point <= 84)
+            {
+                Console.WriteLine("B");
+            }
+            else if (point >=85 || point <= 89)
+            {
+                Console.WriteLine("B+");
+            }
+            else if (point >= 90 || point <= 94)
+            {
+                Console.WriteLine("A-");
+            }
+            else if (point >= 95 || point <= 100)
+            {
+                Console.WriteLine("A");
+            }
+        }
+        public string point
+        {
+            get;
+            set;
+        }
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", point, Mark);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random(DateTime.Now.Second);
+            List<int> Points = new List<int>();
+            for(int i = 0; i < 10; i++)
+            {
+                Points.Add(new Marks
+                {
+                    point = random.Next(0, 100).ToString();
+                });
+            }
+        }
+    }
+
